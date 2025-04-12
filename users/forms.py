fa6384ad1e1604fe29b92af41d9e3977b20f4e47
Supto <abirhasan.supta254@gmail.com>
2025-04-12 20:28:50 +0600

    User Authentication App Create

---
+++ users/forms.py
@@ -0,0 +1,31 @@
+from django.forms import EmailField, EmailInput, PasswordInput, CharField
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class CustomSignUpForm(UserCreationForm):
+    email = EmailField(widget=EmailInput(attrs={'class': 'input', 'placeholder': 'Email'}))
+    password1 = CharField(widget=PasswordInput(attrs={'class': 'input', 'placeholder': 'Password'}))
+    password2 = CharField(widget=PasswordInput(attrs={'class': 'input', 'placeholder': 'Confirm Password'}))
+
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['email']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+
+class CustomLogInForm(AuthenticationForm):
+    username = EmailField(widget=EmailInput(attrs={'class':'input', 'placeholder':'Email'}))
+    password = CharField(widget=PasswordInput(attrs={'class':'input', 'placeholder':'Password'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = "Email"
---
+++ users/views.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from users.forms import CustomSignUpForm, CustomLogInForm
+
+@login_required(login_url='login')
+def home(request):
+    user = 'User'
+    if request.user.is_authenticated:
+        user = request.user.username
+    return render(request, 'users/home.html', {'username': user})
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = CustomSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Error signing up. Please check your details.')
+    else:
+        form = CustomSignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = CustomLogInForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user:
+                login(request, user)
+                return redirect('home')
+        messages.error(request, 'Invalid email or password')
+    else:
+        form = CustomLogInForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
