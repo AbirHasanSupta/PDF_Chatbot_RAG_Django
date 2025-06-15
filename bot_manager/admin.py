ebff0bc8d9b64f29368f30ad705b9e35a884c658
Supto <abirhasan.supta254@gmail.com>
2025-06-15 21:28:30 +0600

    Add Bot and ChatSession model in Admin panel

--- bot_manager/admin.py
+++ bot_manager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bot
 
-# Register your models here.
+@admin.register(Bot)
+class BotAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'user', 'created_at']
+    list_filter = ['name', 'user']
+    search_fields = ['name', 'description']
+    sortable_by = ['created_at']
+
--- chat_manager/admin.py
+++ chat_manager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ChatSession
 
-# Register your models here.
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'created_at', 'bot']
+    list_filter = ['user', 'bot', 'title']
+    search_fields = ['title']
+    sortable_by = ['created_at']
